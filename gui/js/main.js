b8a04e95bf8532d0b8de17ea100493fd175f8a7e
Sean Collins <scollinspt@tutanota.com>
2025-01-23 05:33:46 -0500

    removal of the “SEM-like” style

--- gui/js/main.js
+++ gui/js/main.js
@@ -15,8 +15,6 @@
 *   along with this program; if not, write to the Free Software
 *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA. */
 
-console.log("main.js is loaded and running");
-
 
 var GUI = {
 	view_mode : "DAG",
