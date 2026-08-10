03e0832a8f1e9265fcb7f9874c18a28005e6daf2
Sean Collins <scollinspt@tutanota.com>
2026-08-10 07:51:02 -0400

    Removed DAGitty legacy files for new project vision

--- package.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Make a ZIP package containing
-# the current version of the Models4PT GUI
-# and store it in a file called Models4PT.zip
-
-( cd jslib/ ; make )
-( cd gui ; zip -r ../Models4PT.zip * )
-
--- r/tests/testthat/testTetrads.R
+++
@@ -1,9 +0,0 @@
-
-test_that(
-	"two-factor model",{
-		g <- dagitty("dag{{x1 x2 x3 x4} <- x <-> y -> {y1 y2 y3 y4}}")
-		latents(g) <- c("x","y")
-		expect_equal( nrow(vanishingTetrads(g)), 138 )
-	}
-)
-
